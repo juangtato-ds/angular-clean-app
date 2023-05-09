af2cfc3ca93e68f4637d74f7749355a23a9138f2
Juan <juan@dataspartan.com>
2023-05-09 20:12:14 +0200

    chore: stackblitz compatibility

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// Hack to run this on stackblitz
+import 'zone.js';
+import 'zone.js/dist/long-stack-trace-zone.js';
+
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
